117b7c2bc69c5bdbe1bd840c8330948704dc9d45
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-12-10 23:17:11 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [{ name: "Linda" }, { name: "Frank" }, { name: "Jacky" }],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.name}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
