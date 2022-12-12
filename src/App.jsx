2f1624fd7ba8f2619d767af4ceca0ccce38f5ce8
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-12-12 17:19:31 +0900

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,51 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  //1
   constructor() {
     super();
 
     this.state = {
-      monsters: [{ name: "Linda" }, { name: "Frank" }, { name: "Jacky" }],
+      monsters: [],
     };
   }
 
+  //componentDidMount只RUN 1次(DOM ノードを必要とする初期化はここで行われる)
+  //所以好快
+  componentDidMount() {
+    //3
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users }; //return返個名去state
+          },
+          () => console.log(this.state)
+        )
+      );
+  }
+
   render() {
+    //2
+
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholden="search monsters"
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) =>
+              monster.name.toLocaleLowerCase().includes(searchString)
+            );
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
+
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.name}>{monster.name}</h1>;
         })}
